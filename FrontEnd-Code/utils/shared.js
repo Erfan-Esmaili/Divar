a61fecf5e8f9bc383878fc03653a72a0d42f7160
erfan <erfan.esmaili1382@gmail.com>
2025-09-06 01:41:20 +0330

    fix-icons

--- FrontEnd-Code/utils/shared.js
+++ FrontEnd-Code/utils/shared.js
@@ -30,7 +30,7 @@ const getAndShowSocialsMedia = async () => {
           `
             
               <a href="${social.link}" class="sidebar__icon-link">
-    <img src="${social.icon.filename}" alt="${social.name}" class="sidebar__icon bi bi-twitter">
+    <img src="${social.icon}" alt="${social.name}" class="sidebar__icon bi bi-twitter">
   </a>
             
             
